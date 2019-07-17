0b3b0b1777207076270dd802bb96c2b2d0244630
alexbeams1898 <beamsa1@hawkmail.newpaltz.edu>
2019-07-17 12:29:26 -0400

    Changed styling and added comments to CSS and app.js

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
+/* REQUIRE STATEMENTS */
 const express = require("express");
 const bodyParser = require("body-parser");
 const twilio = require("twilio");
-const app = express();
+const app = express(); // express object
 
+/* MY TWILIO ACCOUNT INFO FOR USING THE API */
 var accountSid = "AC81e5db08492c762a3ff114f5a633614f";
 var authToken = "0327f1092a4860a4a2574d71cb006c75";
+
+/* CREATE NEW TWILIO OBJECT */
 var client = new twilio(accountSid, authToken);
 
+/* USES THE INDEX.EJS FILE AS A VIEW */
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 
+/* LOADS THE PAGE */
 app.get("/", function(req, res) {
   res.render("index");
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/", function(req, res) {
   let toNumber = req.body.toNumber;
   let fromNumber = req.body.fromNumber;
@@ -29,20 +34,6 @@ app.post("/", function(req, res) {
   res.render("index");
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
-});
+app.listen(3000);
 
-// var accountSid = "AC81e5db08492c762a3ff114f5a633614f";
-// var authToken = "0327f1092a4860a4a2574d71cb006c75";
-
-// var twilio = require("twilio");
-// var client = new twilio(accountSid, authToken);
-
-// client.messages
-//   .create({
-//     body: "Hello from Node",
-//     to: "+15184918838", // Text this number
-//     from: "+15185011128" // From a valid Twilio number
-//   })
-//   .then(message => console.log(message.sid));
+// Test Numbers: +15185011128 +15184918838
